c2994902bccb8c1e3b03ab2ad90e014d30836cd8
shashank <shashank.srivastava5523@gmail.com>
2022-10-25 14:19:42 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -47,40 +47,7 @@ function App() {
 
     // "heroku-postbuild": "cd client && npm install && npm install --only=dev --no-shrinkwrap && npm run build",
 
-    // "scripts": {
-    //   "server": "nodemon index.js",
-    //   "start": "nodemon index.js",
-    //   "build": "run build:**",
-    //   "start:lambda": "netlify-lambda serve client",
-    //   "build:lambda": "netlify-lambda build client",
-    //   "deploy:draft": "netlify deploy --dir=build --functions=functions",
-    //   "deploy:prod": "netlify deploy --dir=build --functions=functions --prod"
-  
-    // },
-
     
-  // "scripts": {
-  //   "start": "react-scripts start",
-  //   "build": "react-scripts build",
-  //   "test": "react-scripts test",
-  //   "eject": "react-scripts eject",
-  //   "client": "cd client && yarn start",
-  //   "dev": "concurrently --kill-others-on-fail \"yarn client\"",
-  //   "dev:server": "cd client && yarn build && cd .. && yarn start",
-  //   "heroku-postbuild": "cd client && npm install && npm install --only=dev --no-shrinkwrap && npm run build" 
-  // },
-
-
-  // "start:lambda": "netlify-lambda serve client",
-  // "build:lambda": "netlify-lambda build client",
-  // "deploy:draft": "netlify deploy --dir=build --functions=functions",
-  // "deploy:prod": "netlify deploy --dir=build --functions=functions --prod",
-
-      // "dev": "concurrently --kill-others-on-fail \"yarn client\"",
-    // "dev:server": "cd client && yarn build && cd .. && yarn start",
-    // "heroku-postbuild": "cd client && npm install && npm install --only=dev --no-shrinkwrap " 
-    // https://facebookapii.herokuapp.com/
-
 export default App;
 
 
